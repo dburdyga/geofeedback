a6f44d7fa039536911fd3dddf3f9e4f824867027
Daria <dariaburdyga@gmail.com>
2019-06-24 00:30:25 +0300

    new files

---
+++ src/js/popup.js
@@ -0,0 +1,109 @@
+import render from './../templates/popup.hbs';
+
+var feedbacksArray = [];
+
+function openPopup(obj, myMap, position, clusterer, hintContent) {
+    var popup = document.querySelector('.popup');
+
+    popup.style.display = 'block';
+    popup.innerHTML = render();
+    popup.style.position = 'absolute';
+    popup.style.top = position[1] + 'px';
+    popup.style.left = position[0] + 'px';
+
+    addFeedback(obj, myMap, position, clusterer, popup, hintContent);
+
+    closePopup(popup);
+}
+
+function addFeedback(obj, myMap, position, clusterer, popup, hintContent) {
+    var inputName = document.querySelector('.form__name');
+    var inputPlace = document.querySelector('.form__place');
+    var inputText = document.querySelector('.form__text');
+    var addButton = document.querySelectorAll('.footer__add')[document.querySelectorAll('.footer__add').length - 1];
+
+    var headerAddress = document.querySelector('.header__address-text');
+
+    headerAddress.innerHTML = obj.address;
+
+    var feedbacks = document.querySelector('.feedbacks');
+    var feedback = document.createElement('li');
+
+    feedback.classList.add('feedback');
+    feedback.innerHTML = hintContent;
+    feedbacks.appendChild(feedback);
+
+    addButton.addEventListener('click', () => {
+        if (inputName.value && inputPlace.value && inputText.value) {
+            var feedback = document.createElement('li');
+
+            var name = document.createElement('div');
+            var place = document.createElement('div');
+            var text = document.createElement('div');
+            var day = document.createElement('div');
+            var firstLine = document.createElement('div');
+
+            name.innerHTML = inputName.value;
+            place.innerHTML = inputPlace.value;
+            text.innerHTML = inputText.value;
+
+            var date = new Date();
+
+            day.innerHTML = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+
+            feedback.classList.add('feedback');
+            name.classList.add('feedback__name');
+            place.classList.add('feedback__place');
+            text.classList.add('feedback__text');
+            firstLine.classList.add('feedback__firstLine');
+
+            firstLine.appendChild(name);
+            firstLine.appendChild(place);
+            firstLine.appendChild(day);
+
+            feedback.appendChild(firstLine);
+            feedback.appendChild(text);
+
+            feedbacks.appendChild(feedback);
+
+            inputName.value = '';
+            inputPlace.value = '';
+            inputText.value = '';
+
+            placemarks(obj, myMap, position, clusterer, popup);
+            feedbacksArray.push(feedback);
+        } else {
+            alert('Заполните все поля!')
+        }
+    })
+}
+
+function placemarks(obj, myMap, position, clusterer, popup) {
+    var placemark = new ymaps.Placemark(obj.coords, {
+        hintContent: popup.children[1].lastChild.innerHTML,
+        balloonContent: obj.address + popup.children[1].lastChild.innerHTML
+    }, {
+        preset: 'islands#darkOrangeDotIcon',
+        openHintOnHover: false
+    });
+
+    myMap.geoObjects.add(placemark);
+    clusterer.add(placemark);
+
+    placemark.events.add('click', () => {
+        openPopup(obj, myMap, position, clusterer, placemark.properties._data.hintContent);
+    })
+}
+
+function closePopup(popup) {
+    var closeButton = document.querySelector('.header__close');
+
+    closeButton.addEventListener('click', () => {
+        popup.style.display = 'none';
+        popup.innerHTML = '';
+    })
+}
+
+export {
+    openPopup
+}
--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,13 +1,50 @@
-function mapInit() {
-  
-  ymaps.ready(() => {
-    console.log("111");
+import { openPopup } from './../js/popup';
 
-    let map = new ymaps.Map("map", {
-      center: [55.76, 37.48], // Moscow
-      zoom: 7
-    })
-  })
+function mapInit() {
+  ymaps.ready(() => {
+    var myMap = new ymaps.Map('map', {
+      center: [55.76, 37.64], // Москва
+      zoom: 12,
+      controls: ['zoomControl'],
+      behaviors: ['drag', 'dblClickZoom', 'scrollZoom']
+    }, {
+      searchControlProvider: 'yandex#search',
+      geoObjectOpenBalloonOnClick: false
+    });
+
+    var clusterer = new ymaps.Clusterer({
+      preset: 'islands#invertedDarkOrangeClusterIcons',
+      clusterDisableClickZoom: true,
+      openBalloonOnClick: true,
+      groupByCoordinates: false,
+      clusterBalloonContentLayout: 'cluster#balloonCarousel'
+    });
+
+    myMap.geoObjects.add(clusterer);
+
+    myMap.events.add('click', function(e) {
+      var coords = e.get('coords');
+      var geoCoords = ymaps.geocode(coords);
+      var position = e.get('position');
+
+      geoCoords.then(res => {
+        var obj = {};
+
+        obj.coords = coords;
+        obj.address = res.geoObjects.get(0).properties.get('text');
+        obj.comments = [];
+
+        if (position[1] > window.screen.availHeight - 526) {
+          position[1] = window.screen.availHeight - 526;
+        }
+        if (position[0] > window.screen.availWidth - 380) {
+          position[0] = window.screen.availWidth - 760;
+        }
+
+        openPopup(obj, myMap, position, clusterer, '');
+      });
+    });
+  });
 }
 
 export {
