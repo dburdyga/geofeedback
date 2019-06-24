8375ab8ec72b820d40ca586aee815046eda18f6c
Daria <dariaburdyga@gmail.com>
2019-06-24 13:32:51 +0300

    style changhes

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -1,52 +1,52 @@
 import { openPopup } from './../js/popup';
 
 function mapInit() {
-  ymaps.ready(() => {
-    var myMap = new ymaps.Map('map', {
-      center: [55.76, 37.64], // Москва
-      zoom: 12,
-      controls: ['zoomControl'],
-      behaviors: ['drag', 'dblClickZoom', 'scrollZoom']
-    }, {
-      searchControlProvider: 'yandex#search',
-      geoObjectOpenBalloonOnClick: false
+    ymaps.ready(() => {
+        var myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64], // Москва
+            zoom: 12,
+            controls: ['zoomControl'],
+            behaviors: ['drag', 'dblClickZoom', 'scrollZoom']
+        }, {
+            searchControlProvider: 'yandex#search',
+            geoObjectOpenBalloonOnClick: false
+        });
+
+        var clusterer = new ymaps.Clusterer({
+            preset: 'islands#invertedDarkOrangeClusterIcons',
+            clusterDisableClickZoom: true,
+            openBalloonOnClick: true,
+            groupByCoordinates: false,
+            clusterBalloonContentLayout: 'cluster#balloonCarousel'
+        });
+
+        myMap.geoObjects.add(clusterer);
+
+        myMap.events.add('click', function(e) {
+            var coords = e.get('coords');
+            var geoCoords = ymaps.geocode(coords);
+            var position = e.get('position');
+
+            geoCoords.then(res => {
+                var obj = {};
+
+                obj.coords = coords;
+                obj.address = res.geoObjects.get(0).properties.get('text');
+                obj.comments = [];
+
+                if (position[1] > window.screen.availHeight - 526) {
+                    position[1] = window.screen.availHeight - 526;
+                }
+                if (position[0] > window.screen.availWidth - 380) {
+                    position[0] = window.screen.availWidth - 760;
+                }
+
+                openPopup(obj, myMap, position, clusterer, '');
+            });
+        });
     });
-
-    var clusterer = new ymaps.Clusterer({
-      preset: 'islands#invertedDarkOrangeClusterIcons',
-      clusterDisableClickZoom: true,
-      openBalloonOnClick: true,
-      groupByCoordinates: false,
-      clusterBalloonContentLayout: 'cluster#balloonCarousel'
-    });
-
-    myMap.geoObjects.add(clusterer);
-
-    myMap.events.add('click', function(e) {
-      var coords = e.get('coords');
-      var geoCoords = ymaps.geocode(coords);
-      var position = e.get('position');
-
-      geoCoords.then(res => {
-        var obj = {};
-
-        obj.coords = coords;
-        obj.address = res.geoObjects.get(0).properties.get('text');
-        obj.comments = [];
-
-        if (position[1] > window.screen.availHeight - 526) {
-          position[1] = window.screen.availHeight - 526;
-        }
-        if (position[0] > window.screen.availWidth - 380) {
-          position[0] = window.screen.availWidth - 760;
-        }
-
-        openPopup(obj, myMap, position, clusterer, '');
-      });
-    });
-  });
 }
 
 export {
-  mapInit
+    mapInit
 }
